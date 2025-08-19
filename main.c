a0603f33d11c3a16bf49e65cb58acfc3b739be50
mahirajput13 <Mahirajput9528@gmail.com>
2025-08-19 20:59:55 +0530

    calculateAreaPerimeterOfRectangle

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
-//enter any number provided by user
+// calculate area and perimeter of rectangle
+// width and length is entered by the user
+
 #include <stdio.h>
 int main(void)
 {
-    int n;
+    double l,b,p,a;
     
-    //taking input by the user
-    printf("enter any number to print : \n");
-    scanf("%d", &n);
+    //taking input
+    printf("enter the length of the rectangle : ");
+    scanf("%lf", &l);
+    printf("\nenter  the width of the rectangle : ");
+    scanf("%lf", &b);
     
-    //displaying output
-    printf("number entered by you is %d \n", n);
+    //calculating values
+    p = 2.0*(l + b);
+    a = (l*b);
+    
+    //displaying result/output
+    printf("\nperimeter of the rectangle is %lf ", p);
+    printf("\narea of the rectangle is %lf \n", a);
     
     return 0;
+    
 }
-
-
