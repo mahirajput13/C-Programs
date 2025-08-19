49acc67d2d84956e8f0c2234159dbaa0a5280a1f
mahirajput13 <Mahirajput9528@gmail.com>
2025-08-19 21:02:01 +0530

    implementSignFunction

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-// determine if the number entered by user is even or odd
+// program to implement the sign function
+// determine number is positive or negative
 
 #include <stdio.h>
 int main(void)
@@ -6,17 +7,22 @@ int main(void)
     int num;
     
     //taking input from the user
-    printf("\nenter any number to check if it is even or odd : ");
+    printf("enter any number : ");
     scanf("%d", &num);
     
     //applying condition and displaying output
-    if((num%2)==0)
+    if(num>0)
     {
-        printf("\nnumber entered by you is even\n");
+        printf("\nentered number is positive that is +%d\n", num);
+    }
+    else if (num==0)
+    {
+        printf("\nentered number is neither positive nor negative.\nentered number is %d\n", num);
+    
     }
     else
     {
-        printf("number entered by you is odd\n");
+        printf("entered number is negative.\nthe number is %d \n", num);
     }
     
     return 0;
