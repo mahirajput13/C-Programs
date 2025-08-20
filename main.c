a12ea6827837df0c496484810d56bc27f6132d56
mahirajput13 <Mahirajput9528@gmail.com>
2025-08-20 20:02:50 +0530

    passByReferenceExample

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
-// malloc example
-
+// example of pass by reference
 #include <stdio.h>
 #include <stdlib.h>
+
+//creating function to swap the value
+void swap(int *x, int *y)
+{
+    int temp;
+    
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    
+    return;
+}
+
+//creating main function
 int main(void)
 {
     
-    int *x = malloc(sizeof(int));
-    *x = 54;
-    free(x);
+    int a = 100, b=200;
+    
+    //printing values before swapping
+    printf("before swapping\n");
+    printf("the value of a %d\n",a);
+    printf("the value of b %d\n",b);
+    
+    printf("\nafter swapping\n");
+    
+    //calling swap function
+    swap(&a,&b);
+    printf("the value of a %d\n",a);
+    printf("the value of b %d\n",b);
+    
     return 0;
+    
 }
